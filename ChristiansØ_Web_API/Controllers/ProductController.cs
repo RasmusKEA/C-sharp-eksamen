eda097a1adb3fecf3052735ea4c9ac6762e27856
Rasmus Møller <rasm937k@stud.kea.dk>
2021-11-17 05:13:59 +0100

    lll

---
+++ ChristiansØ_Web_API/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ChristiansØ_Web_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class ProductController : ControllerBase
+    {
+        private string[] test = new[] {"string1", "string2"};
+
+        [HttpGet]
+        public IEnumerable<string> GetProducts()
+        {
+            return test;
+        }
+
+        [HttpGet("{id}")]
+        public string getID(int id)
+        {
+            return test[id];
+        }
+
+        [HttpPost]
+        public IEnumerable<string> Post([FromBody]IEnumerable<string> value)
+        {
+            foreach (var s in value)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.WriteLine("First " + value.ToArray()[0]);
+
+            return value;
+        }
+    }
+}
---
+++ ChristiansØ_Web_API/Controllers/UserController.cs
@@ -0,0 +1,126 @@
+﻿using System.Data;
+using ChristiansØ_Web_API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ChristiansØ_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select userid, username, fullname, password from usertable
+                ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UserAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand mySqlCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = mySqlCommand.ExecuteReader();
+                    table.Load(myReader);
+                    
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(User user)
+        {
+            string query = @"insert into usertable (username, fullname, password) values (@Username, @FullName, @Password)";
+            
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UserAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand mySqlCommand = new MySqlCommand(query, mycon))
+                {
+                    mySqlCommand.Parameters.AddWithValue("@Username", user.Username);
+                    mySqlCommand.Parameters.AddWithValue("@FullName", user.FullName);
+                    mySqlCommand.Parameters.AddWithValue("@Password", user.Password);
+                    
+                    myReader = mySqlCommand.ExecuteReader();
+                    table.Load(myReader);
+                    
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+        
+        [HttpPut]
+        public JsonResult Put(User user)
+        {
+            string query = @"update usertable
+                            set username = @Username
+                            where userid = @UserId";
+            
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UserAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand mySqlCommand = new MySqlCommand(query, mycon))
+                {
+                    mySqlCommand.Parameters.AddWithValue("@Userid", user.UserId);
+                    mySqlCommand.Parameters.AddWithValue("@Username", user.Username);
+                    
+                    myReader = mySqlCommand.ExecuteReader();
+                    table.Load(myReader);
+                    
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+        
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from usertable
+                            where userid = @UserId";
+            
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UserAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand mySqlCommand = new MySqlCommand(query, mycon))
+                {
+                    mySqlCommand.Parameters.AddWithValue("@UserId", id);
+
+                    myReader = mySqlCommand.ExecuteReader();
+                    table.Load(myReader);
+                    
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
